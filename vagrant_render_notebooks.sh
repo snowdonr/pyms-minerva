bde9b968720f4fe19748a04cfb71f43a580ba687
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-16 13:39:38 +0100

    Make vagrant_render_notebooks.sh executable, and install from local source rather than PyPI.

--- vagrant_render_notebooks.sh
+++ vagrant_render_notebooks.sh
@@ -5,7 +5,7 @@ git config --global user.name "Dominic Davis-Foster"
 
 # Install requirements
 sudo apt update
-sudo apt install python3-venv python3-pip || exit 1
+sudo apt install -y python3-venv python3-pip || exit 1
 
 # Create and activate venv
 python3 -m venv /home/vagrant/venv
@@ -13,19 +13,23 @@ source /home/vagrant/venv/bin/activate || exit 1
 
 # Install remaining requirements
 python3 -m pip install pip setuptools wheel --upgrade || exit 1
-python3 -m pip install PyMassSpec --upgrade || exit 1
 python3 -m pip install nbconvert jupyter-client ipykernel --upgrade  || exit 1
+python3 -m pip install -r requirements.txt --upgrade || exit 1
+python3 -m pip install . --upgrade || exit 1
 
 # Change to Notebooks branch
 git checkout -b Notebooks-v2 || exit 1
 
 cd pyms-demo/jupyter || exit 1
 
+# Run once to ensure output files exist
+jupyter nbconvert --to notebook --inplace --execute Multiple_Experiments.ipynb
+
 # Render notebooks and stage
 for file in *.ipynb; do
   jupyter nbconvert --clear-output --inplace "$file"
   jupyter nbconvert --to notebook --inplace --execute "$file"
-  git stage "$file".ipynb
+  git stage "$file"
 done
 
 # Commit and push
