dca6777316fe5d14c4ad896476d2045416001a7f
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-04-11 09:13:02 +0100

    Update documentation configuration.

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -5,16 +5,18 @@
 # stdlib
 import os
 import re
-import sys
-
-sys.path.append(os.path.abspath('.'))
-sys.path.append(os.path.abspath(".."))
 
 # 3rd party
-import ipynb2rst
+from sphinx_pyproject import SphinxConfig
 
-# this package
-from __pkginfo__ import __version__
+config = SphinxConfig()
+
+# stdlib
+import sys
+
+sys.path.extend((os.path.abspath('.'), os.path.abspath("..")))
+# 3rd party
+import ipynb2rst
 
 nitpicky = True
 exclude_patterns = ["../pyms-demo/old/", "demo_rst/*.rst", "todo/", "chapter09.rst", "chapter10.rst"]
@@ -22,21 +24,11 @@ nbsphinx_input_prompt = "In [%s]:"
 
 github_username = "PyMassSpec"
 github_repository = "PyMassSpec"
-github_url = f"https://github.com/{github_username}/{github_repository}"
-
-rst_prolog = f""".. |pkgname| replace:: PyMassSpec
-.. |pkgname2| replace:: ``PyMassSpec``
-.. |browse_github| replace:: `Browse the GitHub Repository <{github_url}>`__
-"""
-
 author = "PyMassSpec Authors"
-project = "PyMassSpec".replace('_', '-')
-slug = re.sub(r'\W+', '-', project.lower())
-release = version = __version__
-copyright = "2019-2021 Dominic Davis-Foster"  # pylint: disable=redefined-builtin
+project = "PyMassSpec"
+copyright = "2019-2021 Dominic Davis-Foster"
 language = "en"
 package_root = "pyms"
-
 extensions = [
 		"sphinx_toolbox",
 		"sphinx_toolbox.more_autodoc",
@@ -61,65 +53,38 @@ extensions = [
 		"enum_tools.autoenum",
 		"sphinx.ext.autosectionlabel",
 		]
-
 sphinxemoji_style = "twemoji"
-todo_include_todos = bool(os.environ.get("SHOW_TODOS", 0))
 gitstamp_fmt = "%d %b %Y"
-
 templates_path = ["_templates"]
 html_static_path = ["_static"]
 source_suffix = ".rst"
 master_doc = "index"
 suppress_warnings = ["image.nonlocal_uri"]
 pygments_style = "default"
-
-intersphinx_mapping = {
-		"python": ("https://docs.python.org/3/", None),
-		"sphinx": ("https://www.sphinx-doc.org/en/stable/", None),
-		}
-
 html_theme = "domdf_sphinx_theme"
-html_theme_options = {"includehidden": False, "logo_only": False}
 html_theme_path = ["../.."]
-html_show_sourcelink = True  # True will show link to source
-
-html_context = {
-		"display_github": True,
-		"github_user": "PyMassSpec",
-		"github_repo": "PyMassSpec",
-		"github_version": "master",
-		"conf_py_path": "/doc-source/",
-		}
-htmlhelp_basename = slug
-
-latex_documents = [("index", f'{slug}.tex', project, author, "manual")]
-man_pages = [("index", slug, project, [author], 1)]
-texinfo_documents = [("index", slug, project, author, slug, project, "Miscellaneous")]
-
-toctree_plus_types = {
+html_show_sourcelink = True
+toctree_plus_types = [
 		"class",
+		"confval",
+		"data",
+		"directive",
+		"enum",
+		"exception",
+		"flag",
 		"function",
 		"method",
-		"data",
-		"enum",
-		"flag",
-		"confval",
-		"directive",
-		"role",
-		"confval",
-		"protocol",
-		"typeddict",
 		"namedtuple",
-		"exception",
-		}
-
+		"protocol",
+		"role",
+		"typeddict",
+		]
 add_module_names = False
 hide_none_rtype = True
 all_typevars = True
 overloads_location = "bottom"
 documentation_summary = "Python Toolkit for Mass Spectrometry"
-
-autodoc_exclude_members = [   # Exclude "standard" methods.
+autodoc_exclude_members = [
 		"__dict__",
 		"__class__",
 		"__dir__",
@@ -137,6 +102,41 @@ autodoc_exclude_members = [   # Exclude "standard" methods.
 		"__abstractmethods__",
 		"__hash__",
 		]
+
+github_url = f"https://github.com/{github_username}/{github_repository}"
+
+rst_prolog = f""".. |pkgname| replace:: PyMassSpec
+.. |pkgname2| replace:: ``PyMassSpec``
+.. |browse_github| replace:: `Browse the GitHub Repository <{github_url}>`__
+"""
+
+slug = re.sub(r'\W+', '-', project.lower())
+release = version = config.version
+
+todo_include_todos = bool(os.environ.get("SHOW_TODOS", 0))
+
+intersphinx_mapping = {
+		"python": ("https://docs.python.org/3/", None),
+		"sphinx": ("https://www.sphinx-doc.org/en/stable/", None),
+		}
+
+html_theme_options = {"includehidden": False, "logo_only": False}
+
+html_context = {
+		"display_github": True,
+		"github_user": "PyMassSpec",
+		"github_repo": "PyMassSpec",
+		"github_version": "master",
+		"conf_py_path": "/doc-source/",
+		}
+htmlhelp_basename = slug
+
+latex_documents = [("index", f'{slug}.tex', project, author, "manual")]
+man_pages = [("index", slug, project, [author], 1)]
+texinfo_documents = [("index", slug, project, author, slug, project, "Miscellaneous")]
+
+toctree_plus_types = set(toctree_plus_types)
+
 autodoc_default_options = {
 		"members": None,  # Include all members (methods).
 		"special-members": None,
